03e664f93577144c997a504efa336597a9dd2e50
Mallard_Dash <mjukcent@gmail.com>
2026-02-19 11:12:42 +0100

    agent-file works + some more on main.py

--- database-storage.sql
+++ database-storage.sql
@@ -1,1 +1,41 @@
 --This is the database for Healthlogger 2.0. It is using Mariadb as RDBMS   
+
+CREATE DATABASE IF NOT EXISTS healthlogger;
+USE healthlogger;
+
+CREATE TABLE IF NOT EXISTS users(
+    user_id INT(64) NOT NULL AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    PRIMARY KEY(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS daily_data(
+    entryid INT(64) NOT NULL AUTO_INCREMENT,
+    entry_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    weight FLOAT,
+    waist FLOAT,
+    blood_preasure VARCHAR(25),
+    mental_state VARCHAR,
+    stress INT,
+    PRIMARY KEY(entryid) 
+);
+
+CREATE TABLE IF NOT EXISTS diary(
+    user_id INT(64) NOT NULL,
+    entry_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    content TEXT NOT NULL
+
+);
+
+--The following table is like a memory-cache for the ai-agent so it can remember your previous conversations
+CREATE TABLE IF NOT EXISTS ai_insights (
+    insight_id INT NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    query_hash VARCHAR(64),     
+    user_query TEXT NOT NULL,    
+    ai_response TEXT NOT NULL,  
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    
+    PRIMARY KEY (insight_id),
+    CONSTRAINT fk_user_insight FOREIGN KEY (user_id) REFERENCES users(user_id)
+) ENGINE=InnoDB;
--- main.py
+++ main.py
@@ -2,24 +2,47 @@
 
 import time
 import colorama
-#import Mariadb
+import mariadb
+import sys
+from dotenv import load_dotenv
 
 def database_connection():
-    pass
+    try:
+        conn = mariadb.connect(
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            host="127.0.0.1",
+            port=int(os.getenv("DB_PORT")),
+            database="healthlogger"
+
+        )
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+        sys.exit(1)
 
 def enter_values():
-    weight=float(input("Please enter todays weight in kgs: "))
-    waist=float(input("Please enter the waist in cm: "))
-    blood_preassure=input("Please enter your bloodpreassure and pulse (OVP/UP-PP): ")
-    mental_state=int(input("Enter a number from 1-10 that best describes todays mood (1=shit, 10=hit): "))
-    stress=int(input("Please enter a number from 1-10 how stressed you feel today (1=calm, 10=burnout): "))
-    
-    summary=(f"Your summary is: {weight:1f} kg\n {waist:1f} cm\n {blood_preassure} \n 0--{mental_state}--10 \n 1--{stress}--10")
-    save_values=input("Save values (y/n) ?")
-    if save_values == "y" or "Y":
-        pass
-    elif save_values == "n" or "N":
-        pass
+    try:
+        weight=float(input("Please enter todays weight in kgs: "))
+        waist=float(input("Please enter the waist in cm: "))
+        blood_preasure=input("Please enter your bloodpreasure and pulse (OVP/UP-PP): ")
+        mental_state=int(input("Enter a number from 1-10 that best describes todays mood (1=shit, 10=hit): "))
+        stress=int(input("Please enter a number from 1-10 how stressed you feel today (1=calm, 10=burnout): "))
+        
+        summary=(f"Your summary is: {weight:1f} kg\n {waist:1f} cm\n {blood_preasure} \n 0--{mental_state}--10 \n 1--{stress}--10")
+        save_values=input("Save values (y/n) ?")
+        if save_values.lower() == "y":
+            data = [
+            (weight, waist, blood_preasure, mental_state, stress)
+            ]
+            cur.executemany("INSERT INTO movie VALUES(?, ?, ?, ?, ?)", data)
+            con.commit() 
+        elif save_values.lower() == "n":
+            print("Discarding values...")
+            return
+        else:
+            print("Discarding values...")
+    except ValueError:
+        print("Error! Please enter only numbers for weight/waist/mental_state/stress ")
 
 def show_values():
     pass
@@ -27,12 +50,12 @@ def show_values():
 def sub_menu_diary():
     while True:
         try:
-            diary_menu_choice=int(input("Please make a choice: "))
             print(f"***Diary menu***\n",
                 "1. Show available diaries\n",
                 "2. Write a new entry\n",
                 "3. Print an entry\n",
                 "4. Go back\n")
+            diary_menu_choice=int(input("Please make a choice: "))
             if diary_menu_choice == 1:
                 pass
             elif diary_menu_choice == 2:
@@ -51,7 +74,6 @@ def sub_menu_diary():
 def graph_menu():
     while True:
         try:
-            graph_menu_choice=int(input("Please make a choice: "))
             print(f"***Graph menu***\n",
                 "1. Weight\n",
                 "2. Waist\n",
@@ -59,17 +81,18 @@ def graph_menu():
                 "4. Mental-state\n",
                 "5. Stress\n",
                 "6. Go back\n")
-            if sub_menu_diary == 1:
+            graph_menu_choice=int(input("Please make a choice: "))
+            if graph_menu_choice == 1:
                 pass
-            elif sub_menu_diary == 2:
+            elif graph_menu_choice == 2:
                 pass
-            elif sub_menu_diary == 3:
+            elif graph_menu_choice == 3:
                 pass
-            elif sub_menu_diary == 4:
+            elif graph_menu_choice == 4:
                 pass
-            elif sub_menu_diary == 5:
+            elif graph_menu_choice == 5:
                 pass
-            elif sub_menu_diary == 6:
+            elif graph_menu_choice == 6:
                 print("Going back...")
                 time.sleep(1)
                 break
@@ -89,7 +112,7 @@ def main_menu():
                   "5. Exit\n")
             menu_choice=int(input("Welcome to Healtlogger version 2.0! Please make a menu choice: "))
             if menu_choice == 1:
-                pass
+                enter_values()
             elif menu_choice == 2:
                 sub_menu_diary()
             elif menu_choice == 3:
@@ -104,5 +127,13 @@ def main_menu():
                 print("Wrong input, try again.")
         except ValueError:
             print("Only integers are allowed")
+
+print("Connecting to database...")
+time.sleep(3)
+print("Connection established!")
+database_connection()
+print("Waking up the ai-agent...")
+time.sleep(2)
+print("Ai-agent up and running!")
 main_menu()
 
